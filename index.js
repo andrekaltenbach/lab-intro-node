1b5c5bf81892fe76bfe05bda921bdc060301a93e
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-04-21 16:22:59 +0200

    solve lab

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    if (this.length > 1) {
+      this.length = this.items.length;
+      return this.items.sort((a, b) => a - b);
+    } else {
+      this.length = this.items.length;
+      return this.items;
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((sum, val) => {
+        return (sum += val);
+      });
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      const total = this.items.reduce((sum, val) => {
+        return (sum += val);
+      });
+      return total / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
